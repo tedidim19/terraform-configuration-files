efe1029c6f77fed394a28dcf433ba65ba161fae9
Kristina Zhelyazkova <kzhelyazkova@vmware.com>
2022-08-30 10:01:39 +0300

    Fix machine syntax

--- gcp-machine/main.tf
+++ gcp-machine/main.tf
@@ -1,13 +1,13 @@
 provider "google" {}
 
 resource "google_compute_instance" "krisi-tf" {
-  machine_type = "e2-medium",
-  name = "krisi-tf-machine",
+  machine_type = "e2-medium"
+  name = "krisi-tf-machine"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
-  },
+  }
   network_interface {
     network = "default"
   }
