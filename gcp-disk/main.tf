5530f880d86f154290e4a3def0720563804b1d29
Teodora Dimitrova <tedidim17@gmail.com>
2022-11-01 23:03:51 +0200

    Update main.tf

--- gcp-disk/main.tf
+++ gcp-disk/main.tf
@@ -2,7 +2,7 @@ provider "google" {}
 
 resource "google_compute_disk" "krisi-tf" {
   provider = "google"
-  name = "tf-gcp-disk2"
+  name = "tf-gcp-disk3"
   type = "pd-ssd"
   zone = "europe-west1-b"
 }
