1d25ad8cfaa4bbf7570ccebc5217980bdc39f77e
Teodora Dimitrova <tedidim17@gmail.com>
2022-11-02 00:59:52 +0200

    Update main.tf

--- gcp-machine/main.tf
+++ gcp-machine/main.tf
@@ -2,7 +2,7 @@ provider "google" {}
 
 resource "google_compute_instance" "krisi-tf" {
   machine_type = "e2-medium"
-  name = "tf-machine2"
+  name = "tf-machine99"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
